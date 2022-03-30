c355374b460c637ea440ae06bc049a9378f6d56d
tomashonse <85421857+tomashonse@users.noreply.github.com>
2022-03-29 22:38:12 -0300

    Add files via upload

---
+++ P0/EJ12.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BitCheck(a, b) ((a) & ( 1 << (b))) // Compara si en el bit b de a hay un 0 o un 1
+#define BitToggle(a,b) ( (a) ^= ( 1<<(b) ) ) // Invierte el valor de la posicion b del bit a
+
+ unsigned setbits(unsigned x, int p, int n, unsigned y){
+	 unsigned x1 = x;
+	 if(n <= p){
+		 for(int i = p; i > p-n; i--){
+			 if(BitCheck(x1, i) != BitCheck(y, i)){
+				 BitToggle(x1, i);
+			 }
+		 }
+	 } else{
+		 printf("El valor de p debe ser numericamente mayor al valor de n\n");
+		 return 0;
+	 }
+	 return x1;
+ }
+
+int main(void){
+	setbuf(stdout, 0);
+	unsigned x = 147, y = 177;
+	int p = 3, n = 3;
+	printf("%d", setbits(x, p, n, y));
+	return 0;
+}
